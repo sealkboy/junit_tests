0f45f16efa8a4aab45bda3f98eb5c578c8bbbaa5
Seal Kevin Boy Abad <sealkevinboyabad@gmail.com>
2024-11-18 09:57:58 +0100

    Añadido primer método

--- src/main/java/org/factoriaf5/App.java
+++ src/main/java/org/factoriaf5/App.java
@@ -1,13 +1,10 @@
-package org.factoriaf5;
+public class Example { 
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public int sumar(int a, int b) {
+        return a + b;
     }
+
+    
+
+
 }
